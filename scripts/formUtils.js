5a40edb37431d736ad1c6d0da884d181fdc7e3ba
asettar <achrafsettar8@gmail.com>
2025-11-16 20:46:06 +0100

    fix prefilling expreiences data

--- scripts/formUtils.js
+++ scripts/formUtils.js
@@ -30,21 +30,22 @@ function    addExperienceForm() {
         <h3>Experience</h3>
         <div>
             <label for="">Position</label>
-            <input type="text" name="" class="position-input" placeholder="Enter Position">
+            <input type="text" name="post" class="position-input" placeholder="Enter Position">
         </div>
         <div>
             <label for="">Start Date</label>
-            <input type="date" name="" id="picture-input">
+            <input type="date" name="startDate" id="picture-input">
         </div>
         <div>
             <label for="">End Date</label>
-            <input type="date" name="" id="picture-input">
+            <input type="date" name="endDate" id="picture-input">
         </div>
         <img class = "delete-experience" src = "Images/delete.png">
     `;
     const deleteBtn = newExperience.querySelector('.delete-experience');
     deleteBtn.addEventListener('click', () => deleteExperience(newExperience));
     form.insertBefore(newExperience, addExperienceButton);
+    return newExperience;
 }
 
 export function prefillFormData(employee) {
@@ -54,11 +55,13 @@ export function prefillFormData(employee) {
     });
     roleInput.value = employee["role"];
     // add experiences
-    let experienceForms = form.querySelectorAll('.experience-form');
-    for (let i = 0; i < employee.experience.length; i++) {
-        const currentExperienceInputs = experienceForms[i].querySelectorAll('input');
+    for (let experience of employee.experience) {
+        console.log(experience);
+        const newExperienceForm = addExperienceForm();
+        const currentExperienceInputs = newExperienceForm.querySelectorAll('input');
+        console.log(currentExperienceInputs);
         currentExperienceInputs.forEach(input => {
-            input.value = employee.experience[i][input.name];
+            input.value = experience[input.name];
         });
     }
 }
