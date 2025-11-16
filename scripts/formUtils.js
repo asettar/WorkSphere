0b7e7cdafa119c8c642d4d5a10ad35fd5938f358
asettar <achrafsettar8@gmail.com>
2025-11-16 14:47:54 +0100

    update new experience form style

--- scripts/formUtils.js
+++ scripts/formUtils.js
@@ -1,21 +1,24 @@
+import { createElement } from "react";
+
 const form = document.querySelector('form');
 const mainContent = document.querySelector('.main-sidebar-content');
+const addExperienceButton = document.getElementById('add-experience-btn');
+
 console.log(form);
 console.log(mainContent);
 
-
 export  function    showForm() {
-    console.log("sow-form");
     mainContent.style.display = 'none';
     form.style.display = 'flex';
 }
 
 export function resetAndCloseForm() {
     // remove experiences from form;
-    
     form.style.display = 'none';
+    mainContent.style.display = 'block';
 }
 
-export function prefillFormData() {
-
+export function prefillFormData(employee) {
+    // prefill default inputs 
+    // add experiences
 }
