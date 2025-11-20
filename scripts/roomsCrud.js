7514c916d8352d84542117c3e55a8f0a74e119a4
asettar <achrafsettar8@gmail.com>
2025-11-20 11:05:16 +0100

    add room additon and popu btns events

--- scripts/roomsCrud.js
+++ scripts/roomsCrud.js
@@ -1,30 +1,54 @@
-import { rooms } from "./setup.js";
-const roomPopUp = document.getElementById('room-popup')
+import { rooms, employeesData} from "./setup.js";
+
+const popUpSection = document.getElementById('room-popup-section')
+const popupConfirmBtn = document.getElementById('confirm-btn-room');
+const popupCancelBtn = document.getElementById('cancel-btn-room');
+let currentRoom, currentSelectedEmployees = [];  // to track selected employees on addition event 
 
 function    showPopup() {
-    popUp.style.display = 'grid';
+    console.log("dewpfefowpe");
+    popUpSection.style.display = 'flex';
+}
+
+function    showAvailablesEmployees() {
+
 }
 
 function    closePopup() {
-    popUp.style.display = 'none';
-    popUp.innerHTML = "";
+    popUpSection.style.display = 'none';
+    popUpSection.querySelector('.room-employees-container').innerHTML = "";
+}
+
+function    addSelectedEmployees() {
+    console.log("room confirm clicked");
+    console.log(currentRoom);
+    console.log("currentSelectedEmployees");
+
 }
 
 function    addRoomAdditionButtonEvent(roomCard, roomData) {
     const additionButton = roomCard.querySelector('.add-employee-room');
+   
     additionButton.addEventListener('click', () => {
-        // message if max is attended and return
+        //todo message if max is attended and return
         showPopup();
         showAvailablesEmployees();
-        closePopup();
     })
 }
 
 
 for (let [roomName, roomData] of Object.entries(rooms)) {
-    const roomCard = document.querySelectorAll(roomName);
+    const roomCard = document.getElementById(roomName);
+    console.log(roomCard);
     // todo: update style if it is empty(pale color if it is empty)
-        // updateRoomStyle();
+    // updateRoomStyle();
     // additionEvent
+    // console.log(roo)
     addRoomAdditionButtonEvent(roomCard, roomData);
 }
+
+
+
+// event
+popupConfirmBtn.addEventListener('click', addSelectedEmployees);
+popupCancelBtn.addEventListener('click', closePopup);
